f992d86e11dd4b402c8d848911637a3e6e7debf7
Seven_Tao <siweitao@siweitao.com>
2015-08-04 18:25:21 +0800

    更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,60 @@
 'use strict';
 
-var gulp = require('gulp'),
-	compass = require('gulp-compass'),
-	plumber = require('gulp-plumber'),
-	watch = require('gulp-watch'),
-	browserSync = require('browser-sync').create(),
-	reload = browserSync.reload;
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var reload = browserSync.reload;
+var plumber = require('gulp-plumber');
 
-gulp.task('server', function() {
-	var files = [
-		'builds/*.html',
-		'builds/js/*.js',
-		'builds/css/*.css',
-		'builds/images/*'
-	];
-	browserSync.init(files, {
-		server: 'builds'
-	});
+/**
+ * Compile jade files into HTML
+ */
+gulp.task('templates', function() {
+    return gulp.src('srcs/jade/*.jade')
+        .pipe(plumber())
+        .pipe(jade({
+            pretty: '    '
+        }))
+        .pipe(gulp.dest('builds'))
 });
 
-gulp.task('compass', function() {
-	gulp.src('srcs/scss/*.scss')
-		.pipe(watch('srcs/scss/*.scss'))
-		.pipe(plumber({
-			errorHandler: function(error) {
-				console.log(error.message);
-				this.emit('end');
-			}
-		}))
-		.pipe(compass({
-			css: 'builds/css',
-			sass: 'srcs/scss'
-		}))
-		.on('error', function(err) {})
-		.pipe(gulp.dest('builds/css'));
+/**
+ * Important!!
+ * Separate task for the reaction to `.jade` files
+ */
+gulp.task('jade-watch', ['templates'], reload);
+
+/**
+ * Sass task for live injecting into all browsers
+ */
+gulp.task('sass', function() {
+    return gulp.src("srcs/scss/*.scss")
+        .pipe(plumber({
+            errorHandler: function(error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .on('error', function(err) {
+            // Would like to catch the error here 
+        })
+        .pipe(gulp.dest("builds/css"))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
-gulp.task('default', ['server', 'compass']);
+/**
+ * Serve and watch the scss/jade files for changes
+ */
+gulp.task('default', ['sass', 'templates'], function() {
+
+    browserSync({
+        server: 'builds'
+    });
+
+    gulp.watch('srcs/scss/*.scss', ['sass']);
+    gulp.watch('srcs/jade/*.jade', ['jade-watch']);
+});
