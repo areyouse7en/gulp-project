8857d666ea69f327b77cb8d923c6c98e84f99551
Seven_Tao <373084212@qq.com>
2015-08-10 18:19:03 +0800

    增加了压缩

---
+++ builds/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(function() {
+	$('.clickbtn').click(function(event) {
+		console.log('ok');
+	});
+});
--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,15 @@ var path = require('path');
 var fs = require('fs');
 var data = require('gulp-data');
 var jade = require('gulp-jade');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var rename = require("gulp-rename");
 
 /**
- * Read the Json file and compile Jade files into HTML
+ * 把jade编译成html，编译过程会寻找对应名字的json文件
  */
 gulp.task('templates', function() {
-    return gulp.src('srcs/jade/**/*.jade')
+    return gulp.src('srcs/jade/*.jade')
         .pipe(plumber())
         .pipe(data(function(file) {
             var json = JSON.parse(fs.readFileSync('srcs/json/' + path.basename(file.path, '.jade') + '.json'));
@@ -27,13 +30,12 @@ gulp.task('templates', function() {
 });
 
 /**
- * Important!!
- * Separate task for the reaction to `.jade` files
+ * 监听jade的任务需要跟编译的分开
  */
 gulp.task('jade-watch', ['templates'], reload);
 
 /**
- * Sass task for live injecting into all browsers
+ * 把scss编译成css
  */
 gulp.task('sass', function() {
     return gulp.src("srcs/scss/*.scss")
@@ -44,9 +46,7 @@ gulp.task('sass', function() {
             }
         }))
         .pipe(sass())
-        .on('error', function(err) {
-            // Would like to catch the error here 
-        })
+        .on('error', function(err) {})
         .pipe(gulp.dest("builds/css"))
         .pipe(reload({
             stream: true
@@ -54,7 +54,27 @@ gulp.task('sass', function() {
 });
 
 /**
- * Serve and watch the scss/jade files for changes
+ * 压缩js和css
+ */
+gulp.task('jimin', function() {
+    return gulp.src('builds/js/*.js')
+        .pipe(uglify())
+        .pipe(rename(function(path) {
+            path.basename += '.min'
+        }))
+        .pipe(gulp.dest('builds/compressed/js'));
+});
+gulp.task('cssmin', function() {
+    gulp.src('builds/css/*.css')
+        .pipe(uglifycss())
+        .pipe(rename(function(path) {
+            path.basename += '.min'
+        }))
+        .pipe(gulp.dest('builds/compressed/css'));
+});
+
+/**
+ * 启动服务器，进行监听
  */
 gulp.task('default', ['sass', 'templates'], function() {
 
